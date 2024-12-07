71dbf40c354e12d3a42b388b529bf33c9c8796c0
Avi Kla <31816166+avikla@users.noreply.github.com>
2024-12-07 09:31:34 +0200

    2024-12-07 0935

--- data/Places_14.js
+++ data/Places_14.js
@@ -8,7 +8,7 @@ var json_Places_14 = {
 { "type": "Feature", "properties": { "Hebrew_Nam": "ביר פורדיס", "English_Na": "Bir Furedis", "Height_m": "861", "Link": null }, "geometry": { "type": "Point", "coordinates": [ 35.131891760294266, 31.630162637847963 ] } },
 { "type": "Feature", "properties": { "Hebrew_Nam": "עין קוזייבה", "English_Na": "Ain Kueiziba", "Height_m": "849", "Link": null }, "geometry": { "type": "Point", "coordinates": [ 35.151317828579884, 31.602989097720215 ] } },
 { "type": "Feature", "properties": { "Hebrew_Nam": "עין צאלח", "English_Na": "Ein Salah", "Height_m": "825", "Link": null }, "geometry": { "type": "Point", "coordinates": [ 35.166244, 31.690786 ] } },
-{ "type": "Feature", "properties": { "Hebrew_Nam": "עין בורק", "English_Na": "Ein Borek", "Height_m": "802", "Link": null }, "geometry": { "type": "Point", "coordinates": [ 35.154077599773835, 31.699164747325561 ] } },
+{ "type": "Feature", "properties": { "Hebrew_Nam": "עין בורק", "English_Na": "Ein Borek", "Height_m": "802", "Link": null }, "geometry": { "type": "Point", "coordinates": [ 35.168057356744768, 31.690013550549327 ] } },
 { "type": "Feature", "properties": { "Hebrew_Nam": "עין פרוגה", "English_Na": "Ein Faruga", "Height_m": "766", "Link": null }, "geometry": { "type": "Point", "coordinates": [ 35.173282775428362, 31.687852479002302 ] } },
 { "type": "Feature", "properties": { "Hebrew_Nam": "עין ארטס", "English_Na": "Ein Artes", "Height_m": "696", "Link": null }, "geometry": { "type": "Point", "coordinates": [ 35.186216, 31.688733347170068 ] } },
 { "type": "Feature", "properties": { "Hebrew_Nam": "עין אל מרארה", "English_Na": "Ein El Maghara", "Height_m": "859", "Link": null }, "geometry": { "type": "Point", "coordinates": [ 35.154054, 31.671722 ] } },
