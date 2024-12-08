50eb340b6e9117ad5b56fdefb6f0d1fcdba67aa8
Avi Kla <31816166+avikla@users.noreply.github.com>
2024-12-08 14:47:39 +0200

    2024-12-08 1450

---
+++ data/Siphons_10.js
@@ -0,0 +1,10 @@
+var json_Siphons_10 = {
+"type": "FeatureCollection",
+"name": "Siphons_10",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"xy_coordinate_resolution": 1e-15,
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Name_EN": "Siphon A", "Name_HE": "סיפון A", "Link": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 35.225999928471033, 31.759070064983433 ], [ 35.22591276123773, 31.759547910471962 ] ] ] } },
+{ "type": "Feature", "properties": { "id": null, "Name_EN": "Siphon A", "Name_HE": "סיפון A", "Link": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 35.220814183754619, 31.767288737510444 ], [ 35.221176000205148, 31.766939836440503 ] ] ] } }
+]
+}
