e34a716a4a312c7ba1638c1f99e28c8f0bef8615
Avi Kla <31816166+avikla@users.noreply.github.com>
2024-12-09 20:06:09 +0200

    2024-12-09 2000

---
+++ data/SiphonBridge_11.js
@@ -0,0 +1,14 @@
+var json_SiphonBridge_11 = {
+"type": "FeatureCollection",
+"name": "SiphonBridge_11",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"xy_coordinate_resolution": 1e-15,
+"features": [
+{ "type": "Feature", "properties": { "Name_EN": "Siphon Start", "Name_HE": null, "Link": null }, "geometry": { "type": "Point", "coordinates": [ 35.225078265790387, 31.757071772022147 ] } },
+{ "type": "Feature", "properties": { "Name_EN": "Siphon End", "Name_HE": null, "Link": null }, "geometry": { "type": "Point", "coordinates": [ 35.220878007697856, 31.767002604000531 ] } },
+{ "type": "Feature", "properties": { "Name_EN": "Siphon Start", "Name_HE": null, "Link": null }, "geometry": { "type": "Point", "coordinates": [ 35.193308839988106, 31.698973293046805 ] } },
+{ "type": "Feature", "properties": { "Name_EN": "Bridge End", "Name_HE": null, "Link": null }, "geometry": { "type": "Point", "coordinates": [ 35.222324913058188, 31.771208942165195 ] } },
+{ "type": "Feature", "properties": { "Name_EN": "Bridge Start", "Name_HE": null, "Link": null }, "geometry": { "type": "Point", "coordinates": [ 35.22624126324601, 31.766948732871761 ] } },
+{ "type": "Feature", "properties": { "Name_EN": "Siphon End", "Name_HE": null, "Link": null }, "geometry": { "type": "Point", "coordinates": [ 35.204267861870534, 31.725472036676784 ] } }
+]
+}
