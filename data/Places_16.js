11d02b5e335db45400a491bb30fcddd177636ee4
Avi Kla <31816166+avikla@users.noreply.github.com>
2024-12-13 08:09:55 +0200

    2024-12-13

--- data/Places_16.js
+++ data/Places_16.js
@@ -5,8 +5,8 @@ var json_Places_16 = {
 "xy_coordinate_resolution": 1e-15,
 "features": [
 { "type": "Feature", "properties": { "Hebrew_Nam": "הר הבית", "English_Na": "Temple Mount", "Height_m": "732", "Link": null }, "geometry": { "type": "Point", "coordinates": [ 35.232651935752124, 31.776647244344421 ] } },
-{ "type": "Feature", "properties": { "Hebrew_Nam": "ביר פורדיס", "English_Na": "Bir Furedis", "Height_m": "861", "Link": null }, "geometry": { "type": "Point", "coordinates": [ 35.131891760294266, 31.630162637847963 ] } },
-{ "type": "Feature", "properties": { "Hebrew_Nam": "עין קוזייבה", "English_Na": "Ain Kueiziba", "Height_m": "849", "Link": null }, "geometry": { "type": "Point", "coordinates": [ 35.151317828579884, 31.602989097720215 ] } },
+{ "type": "Feature", "properties": { "Hebrew_Nam": "ביר פורדיס", "English_Na": "Ein Furedis", "Height_m": "861", "Link": null }, "geometry": { "type": "Point", "coordinates": [ 35.131891760294266, 31.630162637847963 ] } },
+{ "type": "Feature", "properties": { "Hebrew_Nam": "עין קוזייבה", "English_Na": "Ein Kueiziba", "Height_m": "849", "Link": null }, "geometry": { "type": "Point", "coordinates": [ 35.151317828579884, 31.602989097720215 ] } },
 { "type": "Feature", "properties": { "Hebrew_Nam": "עין צאלח", "English_Na": "Ein Salah", "Height_m": "825", "Link": null }, "geometry": { "type": "Point", "coordinates": [ 35.166244, 31.690786 ] } },
 { "type": "Feature", "properties": { "Hebrew_Nam": "עין בורק", "English_Na": "Ein Borek", "Height_m": "802", "Link": null }, "geometry": { "type": "Point", "coordinates": [ 35.168057356744768, 31.690013550549327 ] } },
 { "type": "Feature", "properties": { "Hebrew_Nam": "עין פרוגה", "English_Na": "Ein Faruga", "Height_m": "766", "Link": null }, "geometry": { "type": "Point", "coordinates": [ 35.173282775428362, 31.687852479002302 ] } },
